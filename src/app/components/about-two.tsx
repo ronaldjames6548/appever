2fc833f99d6f2bb4799fa55bb5613c0593635ea4
ronaldjames6548 <ronaldjames6548@gmail.com>
2025-02-11 16:06:48 +0500

    Update about-two.tsx

--- src/app/components/about-two.tsx
+++ src/app/components/about-two.tsx
@@ -8,25 +8,23 @@ export default function AboutTwo() {
         <div className="grid md:grid-cols-12 grid-cols-1 items-center gap-6">
             <div className="lg:col-span-5 md:col-span-6 md:order-2 order-1">
                 <div className="pt-6 px-6 rounded-2xl bg-red-500/5 dark:bg-red-500/10 shadow shadow-red-500/20">
-                    <Image src='/images/phone/half-2.png' width={0} height={0} sizes='100vw' style={{width:'100%', height:'auto'}} alt=""/>
+                    <Image src='/images/phone/half-2.avif' width={0} height={0} sizes='100vw' style={{width:'100%', height:'auto'}} alt=""/>
                 </div>
             </div>
 
             <div className="lg:col-span-7 md:col-span-6 md:order-1 order-2">
                 <div className="lg:me-10">
-                    <h6 className="text-red-500 uppercase text-sm font-bold tracking-wider mb-3">Elegant Design</h6>
-                    <h4 className="mb-6 md:text-3xl text-2xl md:leading-normal leading-normal font-bold">Share your photos with <br/> friends easily</h4>
-                    <p className="text-slate-400 max-w-xl">Unleash the power of our platform with a multitude of powerful features, empowering you to achieve your goals.</p>
+                    <h6 className="text-red-500 uppercase text-sm font-bold tracking-wider mb-3">AnimeWitcher (انمي ويتشر)</h6>
+                    <h4 className="mb-6 md:text-3xl text-2xl md:leading-normal leading-normal font-bold">Share your Favorite Anime with <br/> friends easily</h4>
+                    <p className="text-slate-400 max-w-xl">Animewithcer has the feature to Download and Share the favorite anime in your device and with friends</p>
 
                     <ul className="list-none text-slate-400 mt-6">
-                        <li className="mb-1 flex"><i className="mdi mdi-check text-red-500 text-xl me-2"></i> Digital Marketing Solutions for Tomorrow</li>
-                        <li className="mb-1 flex ms-0"><i className="mdi mdi-check text-red-500 text-xl me-2"></i> Our Talented & Experienced Marketing Agency</li>
-                        <li className="mb-1 flex ms-0"><i className="mdi mdi-check text-red-500 text-xl me-2"></i> Create your own skin to match your brand</li>
+                        <li className="mb-1 flex"><i className="mdi mdi-check text-red-500 text-xl me-2"></i> Search Engine in the “Anime Witcher” helps to find any desired content with ease</li>
+                        <li className="mb-1 flex ms-0"><i className="mdi mdi-check text-red-500 text-xl me-2"></i> Classification service enables to specify the category for each content.</li>
+                        <li className="mb-1 flex ms-0"><i className="mdi mdi-check text-red-500 text-xl me-2"></i> Notification service that alerts the user to the presence of updates in the Anime Witcher</li>
                     </ul>
 
-                    <div className="mt-6">
-                        <Link href="" className="hover:text-red-500 dark:hover:text-red-500 after:bg-red-500 dark:text-white transition duration-500 font-medium">Learn More <i className="mdi mdi-arrow-right align-middle"></i></Link>
-                    </div>
+                    
                 </div>
             </div>
         </div>
